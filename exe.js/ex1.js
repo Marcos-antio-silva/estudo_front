a04eea0cf0035185f8efea99bf7a0299ff82d09a
Marcos-antio-silva <157077914+Marcos-antio-silva@users.noreply.github.com>
2024-05-07 14:10:25 -0300

    14:10 terça
    ble

--- exe.js/ex1.js
+++ exe.js/ex1.js
@@ -1,6 +1,6 @@
 var alerta = document.querySelector('.alerta');
-var form = document.querySelector ('.formulario');
-var barra = document.querySelector('glosario')
+var form = document.querySelector ('.gf');
+var barra = document.querySelector('#glosario')
 var enviar_btn = document.querySelector('.btn')
 var containrt = document.querySelector('.lista-container')
 var lista = document.querySelector('.lista')
@@ -10,21 +10,52 @@ let editElement;
 let editFlag = false;
 let editID = "";
 
-form.addEventListener('submit',addItem)
+form.addEventListener('submit',addItem);
 
 function addItem(e) {
     e.preventDefault ();
-    console.log(barra.value)
-    const value = barra.value
-    // const id = new Date(). getTime() .toString();
-
-    // if(value !=='' && editFlag == false ){
-    //     console.log ('item adicionado');
-    // }
-    // else if (value !== '' && editFlag === true){
-    //     console.log('editando');
-    // }
-    // else {
-    //     console.log('vazio');
-    // }
+    const value = barra.value;
+    const id = new Date().getTime().toString();
+    if(value && !editFlag){
+        console.log ('adicionado')
+        // const elemento = document.createElement('article');
+        // //adicioando a classe 
+        // elemento.classList.add('itens');
+        // const atr =  document.createAttribute('data-id');
+        // atr.value = id;
+        // elemento.setAttributeNode(atr);
+        // elemento.innerHTML = `
+        // <p class="titulo">${value}</p>
+        // <div class="btn-conteiner">
+        //     <button type="button" class="editar-btn">
+        //         <i class="fas fa-edit"></i>
+        //     </button>
+        //     <div class="btn-conteiner">
+        //         <button type="button" class="excluirBtn">
+        //             <i class="fas fa-lixo"></i>
+        //     </button>
+        // </div>`;
+        // lista.appendChild(elemento);
+        // barraNotificação('item adcionado', 'sucesso')
+        // container.classList.add('mostrar-container')
+        barraNotificação('foi','sucesso')
+    }
+    else if (value && editFlag){
+        console.log('editando');
+    }
+    else {
+        // barraNotificação('insira uma produto', 'erro' )
+        barraNotificação ('insira um produto na barra','erro')
+    }
 }
+
+//barra de notificação
+
+function barraNotificação(text,x){
+    alerta.innerHTML = text;
+    alerta.classList.add(`alerta-${x}`)
+}
+// setTimeout (function(x){
+//     alerta.innerHTML = '';
+//     alerta.classList.remove(`alerta-${x}`)
+// }(x),1500)
