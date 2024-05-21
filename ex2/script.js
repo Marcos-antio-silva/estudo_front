524d7f5d4a0c9ea5f4ce88db47139597f469a4c3
Marcos-antio-silva <157077914+Marcos-antio-silva@users.noreply.github.com>
2024-05-21 14:45:23 -0300

    bla

--- ex2/script.js
+++ ex2/script.js
@@ -1,3 +1,8 @@
 function caregar (){
     var msg = document.getElementById ()
+    var plano = document.getElementById(fundo)
+    var data = new Date()
+    var hora = data.getTime()
+    msg.innerHTML = `agora são ${hora}`
 }
+
