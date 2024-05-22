d6884102e59807fb8923cce9661ced84a33e0470
Marcos-antio-silva <157077914+Marcos-antio-silva@users.noreply.github.com>
2024-05-21 21:05:43 -0300

    1

--- ex2/script.js
+++ ex2/script.js
@@ -1,8 +1,19 @@
-function caregar (){
-    var msg = document.getElementById ()
-    var plano = document.getElementById(fundo)
+function carregar (){
+    var msg = document.getElementById ('msg')
+    var plano = document.getElementById('fundo')
     var data = new Date()
-    var hora = data.getTime()
-    msg.innerHTML = `agora são ${hora}`
+    var hora = data.getHours()
+    
+    if (hora >= 6 && hora < 15) {
+        plano.style.backgroundImage = 'url("img/dia.jpg")';
+        msg.innerHTML = 'Bom dia';
+    } else if (hora >= 15 && hora < 20) {
+        plano.style.backgroundImage = 'url("img/tarde.jpg")';
+        msg.innerHTML = 'Boa tarde';
+    } else {
+        plano.style.backgroundImage = 'url("img/noite.jpg")';
+        msg.innerHTML = 'Boa noite';
+    }
+
 }
 
