03939a6acfdb9eebd121f6d8a3f7d5021eee89d8
Marcos-antio-silva <157077914+Marcos-antio-silva@users.noreply.github.com>
2024-05-26 10:12:33 -0300

    klj

--- ex2/script.js
+++ ex2/script.js
@@ -5,33 +5,29 @@ var data = new Date()
 var hora = data.getHours()
 var form = document.querySelector ('.gf');
 var barra = document.querySelector('#glosario')
-var enviar_btn = document.querySelector('.btn')
 var containrt = document.querySelector('.lista-container')
 var lista = document.querySelector('.lista')
-var excluirBtn = document.querySelector("excluirBtn")
+var excluirBtn = document.querySelector("reset-btn")
 
-
-function estilo(mensagem,img_fundo,cor_borda,cor,cor2) {
+function estilo(img_fundo,cor_borda,cor) {
     plano.style.backgroundImage = img_fundo ;
     plano.style.backgroundSize = '100%'
-    msg.innerHTML = mensagem;
-    msg.style.color = cor
     bloco.style.backgroundColor = cor_borda
-    bloco.style.boxShadow = `8px 7px 7px ${cor2}`
+    bloco.style.boxShadow = `8px 7px 7px ${cor}`
 }
 
 function carregar (){    
-    if (hora >= 6 && hora < 15) {
+    if (hora >= 0 && hora < 12) {
 
-       estilo('Bom dia','url("img/dia.jpg")',' #00004d','white','black')
+       estilo('url("img/dia.jpg")',' #00004d','black')
 
-    } else if (hora >= 15 && hora < 20) {
+    } else if (hora >= 12 && hora < 18) {
 
-        estilo('Boa tarde','url("img/tarde.jpg")','#4d2600','white','black')
+        estilo('url("img/tarde.jpg")','#4d2600','black')
 
     } else {
 
-        estilo('Boa noite','url("img/noite.jpg")','#000033','white')
+        estilo('url("img/noite.jpg")','#000033','white')
 
     }
 }
@@ -46,8 +42,8 @@ function addItem(e) {
     e.preventDefault ();
     const value = barra.value;
     const id = new Date().getTime().toString();
+
     if(value && !editFlag){
-        console.log ('adicionado')
         const elemento = document.createElement('article');
         //adicioando a classe 
         elemento.classList.add('itens');
@@ -55,26 +51,34 @@ function addItem(e) {
         atr.value = id;
         elemento.setAttributeNode(atr);
         elemento.innerHTML = `
-        <p class="titulo">${value}</p>
-        <div class="btn-conteiner">
-            <button type="button" class="editar-btn">
-                <i class="fas fa-edit"></i>
-            </button>
-            <div class="btn-conteiner">
-                <button type="button" class="excluirBtn">
-                    <i class="fas fa-lixo"></i>
-            </button>
-        </div>`;
+        <p class="titulo">${value}</p>`;
         lista.appendChild(elemento);
-        barraNotificação('item adcionado', 'sucesso')
-        container.classList.add('mostrar-container')
         barraNotificação('foi','sucesso')
-    }
-    else if (value && editFlag){
-        console.log('editando');
+        console.log (value, id)
     }
     else {
         // barraNotificação('insira uma produto', 'erro' )
         barraNotificação ('insira um produto na barra','erro')
     }
+}
+
+//barra de notificação
+
+function barraNotificação(text, x) {
+    var alerta = document.querySelector('.alerta'); // Ensure the alerta element is defined
+    if (!alerta) {
+        console.error('Element with id "alerta" not found.');
+        return;
+    }
+    alerta.innerHTML = text;
+    alerta.classList.add(`alerta-${x}`);
+    
+    setTimeout(function() {
+        alerta.innerHTML = '';
+        alerta.classList.remove(`alerta-${x}`);
+    }, 1500);
+}
+
+function reset(){
+ 
 }
